5cfc2244c5d3f4028645c302cd8d1fbded9d55f4
rcsnjszg <49613681+rcsnjszg@users.noreply.github.com>
2024-11-25 10:32:28 +0100

    init

---
+++ jsonserver/index.js
@@ -0,0 +1,20 @@
+const bodyParser = require('body-parser')
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('data/db.json')
+
+const multer = require('multer')
+const forms = multer()
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use(jsonServer.bodyParser)
+server.use(forms.array())
+server.use(bodyParser.urlencoded({extended: true}))
+
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
