e3a37f51520c33e2e177fa335df43dd12b6e4ab0
Ignácz Dominik Bence <ignacz.dominik@njszg.hu>
2024-12-12 10:43:23 +0100

    Fix missing data from json server

--- jsonserver/index.js
+++ jsonserver/index.js
@@ -14,6 +14,12 @@ server.use(jsonServer.bodyParser)
 server.use(forms.array())
 server.use(bodyParser.urlencoded({extended: true}))
 
+router.render = (req, res) => {
+    res.jsonp({
+        data: res.locals.data
+    })
+}
+
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
